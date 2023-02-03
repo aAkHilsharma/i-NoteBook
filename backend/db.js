3e50fb534573ce64556d32ee1039127f18be905e
akhil <akhilsharma6721@gmail.com>
2023-02-03 21:01:03 +0530

    created a database

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,10 @@
 const { default: mongoose } = require("mongoose");
 
-const mongoURI = "mongodb://localhost:27017"
+const mongoURI = "mongodb://localhost:27017/inotebook"
 mongoose.set('strictQuery', true);
 const connectToMongo= ()=>{
     mongoose.connect(mongoURI, ()=>{
         console.log("Connection success!!");
     })
 }
-
 module.exports = connectToMongo;
