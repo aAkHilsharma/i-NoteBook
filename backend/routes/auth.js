35c5ee38b3e4ca256b11c9335a467dc4c956ccfa
akhil <akhilsharma6721@gmail.com>
2023-02-03 20:19:07 +0530

    added validation to user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const User = require("../models/User");
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-router.post('/', (req, res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
+router.post('/',[
+    body('name', 'Enter a valid name').isLength({min: 2}),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be atleast 5 characters').isLength({min: 5}),
+], (req, res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
     res.send(req.body);
 })
 
