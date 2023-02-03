f4350e35d186775b6982bda1cf78914e2e03b79c
akhil <akhilsharma6721@gmail.com>
2023-02-03 20:22:35 +0530

    created user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -12,7 +12,11 @@ router.post('/',[
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send(req.body);
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,  
+    }).then(user => res.json(user));
 })
 
 module.exports = router;
