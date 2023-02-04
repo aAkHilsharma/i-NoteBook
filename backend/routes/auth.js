fe46f0d0b7bae2fc4c7912704f1973132efa6de4
akhil <akhilsharma6721@gmail.com>
2023-02-04 20:40:17 +0530

    refactored authentication

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,22 +3,24 @@ const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
-router.post('/',[
+router.post('/createuser',[
     body('name', 'Enter a valid name').isLength({min: 2}),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be atleast 5 characters').isLength({min: 5}),
-], (req, res)=>{
+], async (req, res)=>{
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+    let user = User.findOne({email: req.body.email});
+    if(user){
+        return req.status(400).json({error: "Sorry a user with this email already exists"});
+    }
+    user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,  
-    }).then(user => res.json(user))
-    .catch(err=>{console.log(err)
-    res.json({error: 'Please enter valid value', message: err.message})})
+    })
 })
 
-module.exports = router;
+module.exports = router; 
