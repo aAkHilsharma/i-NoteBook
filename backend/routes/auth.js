95a01c7476d0833f230db89bce8bc8b66320d74c
akhil <akhilsharma6721@gmail.com>
2023-02-04 21:05:26 +0530

    added comments

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,32 +1,43 @@
-const express = require('express');
+const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
-const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require("express-validator");
 
-router.post('/createuser',[
-    body('name', 'Enter a valid name').isLength({min: 2}),
-    body('email', 'Enter a valid email').isEmail(),
-    body('password', 'Password must be atleast 5 characters').isLength({min: 5}),
-], async (req, res)=>{
+// create a user using: POST "/api/auth/createuser"
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter a valid name").isLength({ min: 2 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+    // Check if email is unique if not send error 
     try {
-        let user = await User.findOne({email: req.body.email});
-        if(user){
-            return res.status(400).json({error: "Sorry a user with this email already exists"});
-        }
-        user = await User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,  
-        })
-        res.json({"success": "Successful"});
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists" });
+      }
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      res.json({ success: "Successful" });
+      // if above code does not run then give a error
     } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Some error occured try again!")
+      console.error(error.message);
+      res.status(500).send("Some error occured try again!");
     }
-})
+  }
+);
 
-module.exports = router; 
+module.exports = router;
