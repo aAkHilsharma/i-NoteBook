4ae47c2d39694ca86cede5c37e2d185452fee3b7
akhil <akhilsharma6721@gmail.com>
2023-02-04 20:52:35 +0530

    fixed response bug

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -12,7 +12,7 @@ router.post('/createuser',[
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    let user = User.findOne({email: req.body.email});
+    let user = await User.findOne({email: req.body.email});
     if(user){
         return req.status(400).json({error: "Sorry a user with this email already exists"});
     }
@@ -21,6 +21,7 @@ router.post('/createuser',[
         email: req.body.email,
         password: req.body.password,  
     })
+    res.json({"success": "Successful"});
 })
 
 module.exports = router; 
