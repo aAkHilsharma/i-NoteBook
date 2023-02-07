800c030e218eb5a97b1fe675084cc639018c032b
akhil <akhilsharma6721@gmail.com>
2023-02-07 12:28:34 +0530

    response fix

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -98,6 +98,7 @@ router.post("/getuser", fetchuser ,async(req, res)=>{
   try {
     const userId = req.user.id;
     const user = await User.findById(userId).select("-password");
+    res.send(user);
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Internal Server Error");
