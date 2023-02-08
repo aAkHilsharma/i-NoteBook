670491cdee4315a29edcd01383342124af42f2c1
akhil <akhilsharma6721@gmail.com>
2023-02-08 16:12:39 +0530

    created route for notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,12 +1,11 @@
 const express = require("express");
-
+const fetchuser = require('../middleware/fetchuser');
 const router = express.Router();
+const Notes = require("../models/Notes");
 
-router.get("/",(req, res)=>{
-    obj = {
-        example: "Notes"
-    }
-    res.json(obj);
+router.get("/fetchallnotes", fetchuser,async(req, res)=>{
+    const notes = await Notes.find({user: req.user.id});
+    res.json(notes);
 })
 
 module.exports = router
