4776aed9371b45273c94c8252370afb27b603d9b
akhil <akhilsharma6721@gmail.com>
2023-02-10 15:05:28 +0530

    added routes with router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './components/Home';
+import About from './components/About';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Started Working on Front-end of iNoteBook</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/about' element={<About/>}/>
+        </Routes>
+      </BrowserRouter> 
+    </>
   );
 }
 
