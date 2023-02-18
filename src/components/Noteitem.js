ce8c5daf01db7570ccd39803570da5215b88f57b
akhil <akhilsharma6721@gmail.com>
2023-02-18 19:02:11 +0530

    added font awesome

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -8,6 +8,7 @@ const Noteitem = (props) => {
                 <div className="card-body">
                     <h5 className="card-title">{note.title}</h5>
                     <p className="card-text">{note.description}</p>
+                    <i class="fa-regular fa-trash-can"></i>
                 </div>
             </div>
         </div>
