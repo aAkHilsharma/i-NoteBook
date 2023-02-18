e48b8e1bab9823c58c0a68dfb5173066c15a3146
akhil <akhilsharma6721@gmail.com>
2023-02-18 18:51:44 +0530

    refactored noteitem

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const Noteitem = (props) => {
-    const {notes} = props;
+    const {note} = props;
     return (
         <div className='col-md-3 '>            
            <div className="card my-3">
                 <div className="card-body">
-                    <h5 className="card-title">{notes.title}</h5>
-                    <p className="card-text">{notes.description}</p>
+                    <h5 className="card-title">{note.title}</h5>
+                    <p className="card-text">{note.description}</p>
                 </div>
             </div>
         </div>
