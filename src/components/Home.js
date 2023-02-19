03e65401f50b2701c367023b20f14ef78ec206ed
akhil <akhilsharma6721@gmail.com>
2023-02-19 19:35:20 +0530

    create addnote component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,12 @@
 import React, {useContext} from "react"
+import AddNote from "./AddNote";
 import Notes from "./Notes";
 
 
 export default function Home(){
     return(
         <div>
-            <div className="container my-3">
-                <h1>Add a Note</h1>
-                <form>
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" />
-                    </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                </form>
-            </div>
+            <AddNote />
             <Notes />
         </div>
     )
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -4,39 +4,52 @@ import noteContext from "./NoteContext";
 const NoteState = (props) => {
   const notesInitial = [
     {
-      _id: "63es500b9cedd205178169ea6",
-      user: "63e11cefe2dc450c4e6ec357",
-      title: "Daily Routine",
-      description:
+      "_id": "63es500b9cedd205178169ea6",
+      "user": "63e11cefe2dc450c4e6ec357",
+      "title": "Daily Routine",
+      "description":
         "Do these daily: gym, yoga, meditation, web development, data structures, rest",
-      tag: "Development",
-      date: "2023-02-09T14:18:33.886Z",
-      __v: 0,
+      "tag": "Development",
+      "date": "2023-02-09T14:18:33.886Z",
+      "__v": 0,
     },
     {
-      _id: "63e50187dc7e6d3e6aca902a8",
-      user: "63e11cefe2dc450c4e6ec357",
-      title: "Daily Routine",
-      description:
+      "_id": "63e50187dc7e6d3e6aca902a8",
+      "user": "63e11cefe2dc450c4e6ec357",
+      "title": "Daily Routine",
+      "description":
         "Do these daily: gym, yoga, meditation, web development, data structures, rest",
-      tag: "Development",
-      date: "2023-02-09T14:21:59.481Z",
-      __v: 0,
-    },
-    {
-      _id: "63e5fb18d8ab74ca90e1e7ff1",
-      user: "63e11cefe2dc450c4e6ec357",
-      title: "Daily Routine",
-      description:
-        "Do these daily: gym, yoga, meditation, web development, data structures, rest",
-      tag: "Development",
-      date: "2023-02-10T08:06:48.093Z",
-      __v: 0,
-    },
+      "tag": "Development",
+      "date": "2023-02-09T14:21:59.481Z",
+     "__v": 0,
+    }
   ];
   const [notes, setNotes] = useState(notesInitial);
+  // add note
+  const addNote = (title, description, tag)=>{
+    //TODO api call
+    const note = {
+      "_id": "63e5fb18d8ab74ca90e1e7ff1",
+      "user": "63e11cefe2dc450c4e6ec357",
+      "title": "Daily Routine ADDED",
+      "description":
+        "Do these daily: gym, yoga, meditation, web development, data structures, rest",
+      "tag": "Development",
+      "date": "2023-02-10T08:06:48.093Z",
+      "__v": 0,
+    }
+    setNotes(notes.push(note));
+  }
+  // delete note
+  const deleteNote = ()=>{
+
+  }
+  // edit note
+  const editNote = ()=>{
+
+  } 
   return (
-    <noteContext.Provider value={{notes,setNotes}}>
+    <noteContext.Provider value={{notes,setNotes, addNote, deleteNote, editNote}}>
       {props.children}
     </noteContext.Provider>
   );
